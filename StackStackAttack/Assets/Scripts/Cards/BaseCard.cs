94a722be5283c937f2c8eafd83675ee78aeefc71
Kisame712 <kisamehoshigakiuhmw712@gmail.com>
2026-01-19 19:50:32 +0530

    Added music and card scriptable objects

---
+++ StackStackAttack/Assets/Scripts/Cards/BaseCard.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewBaseCard", menuName = "BaseCard")]
+public class BaseCard : ScriptableObject
+{
+    public string elementName;
+    public string elementId;
+    public string cardName;
+    // Damage done by an individual isolated card
+    public int baseAttackPoints;
+
+    // Mapping will be a array of lists per element => card[elementId][cardIndex]
+    public int cardIndex;
+
+    // A global stackMultiplier will be multiplied with this number if the stack has connection/synergy with the last stacked card
+    public float stackMultiplier;
+
+    // Add a sprite for every element to reprent the card type
+    // public Sprite elementSprite
+}
