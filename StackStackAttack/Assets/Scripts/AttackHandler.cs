8fae70242e2e306fc059fc0d4a0dc53ff44b1bb1
Kisame712 <kisamehoshigakiuhmw712@gmail.com>
2026-01-21 17:32:51 +0530

    Added Card Prefab

---
+++ StackStackAttack/Assets/Scripts/AttackHandler.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+public class AttackHandler : MonoBehaviour
+{
+    public event EventHandler OnActionButtonClicked;
+    private Button attackButton;
+
+    private void Awake()
+    {
+        attackButton = GetComponent<Button>();
+    }
+
+    private void Start()
+    {
+        attackButton.onClick.AddListener(() =>
+        {
+            OnActionButtonClicked?.Invoke(this, EventArgs.Empty);
+        });
+    }
+}
