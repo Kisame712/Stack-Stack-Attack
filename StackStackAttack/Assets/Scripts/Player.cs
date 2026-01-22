7bb48d5f9689cc098b9c9cf5c6bb8641c42987bc
Kisame712 <kisamehoshigakiuhmw712@gmail.com>
2026-01-22 19:59:06 +0530

    Added active card management

--- StackStackAttack/Assets/Scripts/Player.cs
+++ StackStackAttack/Assets/Scripts/Player.cs
@@ -3,20 +3,9 @@ using System.Collections.Generic;
 
 public class Player : MonoBehaviour
 {
-    public static Player Instance { private set; get; }
     [SerializeField] private List<BaseCard> playerCards;
     [SerializeField] private int stackLimit;
 
-    private void Awake()
-    {
-        if(Instance != null)
-        {
-            Debug.LogError("There is more than one instance of Player - " + transform);
-            Destroy(gameObject);
-            return;
-        }
-        Instance = this;
-    }
     public List<BaseCard> GetPlayerCards()
     {
         return playerCards;
--- StackStackAttack/Assets/Scripts/UI/ElementCardParent.cs
+++ StackStackAttack/Assets/Scripts/UI/ElementCardParent.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
-
+using System.Collections.Generic;
 public class ElementCardParent : MonoBehaviour
 {
+    [SerializeField] private int elementId;
+    [SerializeField] private Transform cardPrefab;
 
+    private List<BaseCard> elementActiveCards;
     private void OnEnable()
     {
-        
+        elementActiveCards = CardManager.Instance.GetElementActiveCards(elementId);
+
+        Debug.Log(elementActiveCards.Count);
+
+        for (int i = 0; i < elementActiveCards.Count; i++) 
+        {
+            Transform newCard = Instantiate(cardPrefab, transform);
+        }
+            
     }
 
 
     private void OnDisable()
     {
-        
+        foreach(Transform elementCard in transform)
+        {
+            Destroy(gameObject);
+        }
+
+        elementActiveCards.Clear();
     }
 }
